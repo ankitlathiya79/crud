a5fe14c7d45e87223adbae4b8866901a467bcae1
ankitlathiya79 <lathiyaankit99@gmail.com>
2016-08-04 18:51:32 -0700

    third commit

---
+++ database/migrations/2016_08_05_014500_create_cruds_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCrudsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cruds', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('surname');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cruds');
+    }
+}
